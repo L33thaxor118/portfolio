90a9becd9c4edb3b767ae5752609c68c592388c8
Azriel Alvarado <aalvarado@fulcrumgt.com>
2021-03-17 12:00:09 -0500

    set milestones

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -4,9 +4,75 @@ import styles from './styles.css'
 export default function Content() {
   return (
     <div className={styles.content}>
-      <h1>
-        im content
-      </h1>
+      <h1>Web</h1>
+      <h2>cowghost</h2>
+      <h4>frontend</h4>
+      <h4>backend</h4>
+
+      <h1 className={styles.divider}/>
+
+      <h2>this site</h2>
+      <h4>frontend</h4>
+      <h4>backend: none</h4>
+
+      <h1 className={styles.divider}/>
+      <h1 className={styles.divider}/>
+
+      <h1>Hybrid</h1>
+      <h2>wexley (react native)</h2>
+      <h4>frontend</h4>
+      <h4>backend: firebase</h4>
+
+      <h1 className={styles.divider}/>
+
+      <h2>affirmations (electron)</h2>
+      <h4>frontend</h4>
+      <h4>backend: none</h4>
+
+      <h1 className={styles.divider}/>
+
+      <h2>siphon (electron)</h2>
+      <h4>frontend</h4>
+      <h4>backend: none</h4>
+
+      <h1 className={styles.divider}/>
+      <h1 className={styles.divider}/>
+
+      <h1>Native</h1>
+      <h2>android project?</h2>
+      <h4>frontend</h4>
+      <h4>backend</h4>
+
+      <h1 className={styles.divider}/>
+
+      <h2>ios project?</h2>
+      <h4>frontend</h4>
+      <h4>backend</h4>
+
+      <h1 className={styles.divider}/>
+
+      <h2>camera RTS feed android</h2>
+      <h4>frontend</h4>
+      <h4>backend</h4>
+
+      <h1 className={styles.divider}/>
+      <h1 className={styles.divider}/>
+      <h1 className={styles.divider}/>
+      <h1 className={styles.divider}/>
+      <h1 className={styles.divider}/>
+      <h1 className={styles.divider}/>
+      <h1 className={styles.divider}/>
+      <h1 className={styles.divider}/>
+      <h1 className={styles.divider}/>
+      <h1 className={styles.divider}/>
+
+      <h1>shitty school projects</h1>
+      <h2>illinihub</h2>
+      <h2>textbookexchange</h2>
+      <h2>vacay</h2>
+      <h2>deepcyte</h2>
+      <h2>recycle warrior</h2>
+
     </div>
   )
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -5,8 +5,11 @@ import profileImage from '../../assets/alien.jpeg'
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
-      <h1>hi im a sidebar</h1>
       <img className={styles.profileImg} src={profileImage}/>
+      <h3 className={styles.listItemText}>whoami</h3>
+      <h3 className={styles.listItemText}>projects</h3>
+      <h3 className={styles.listItemText}>symlinks</h3>
+      <h3 className={styles.listItemText}>contact</h3>
     </div>
   )
 }
