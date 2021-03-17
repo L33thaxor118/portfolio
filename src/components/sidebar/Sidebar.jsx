a1351c3ecd4581c0ce492d2b54c063611f74e56a
Azriel Alvarado <aalvarado@fulcrumgt.com>
2021-03-17 00:52:53 -0500

    add images and fonts

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './styles.css'
+import profileImage from '../../assets/alien.jpeg'
 
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <h1>hi im a sidebar</h1>
+      <img className={styles.profileImg} src={profileImage}/>
     </div>
   )
 }
