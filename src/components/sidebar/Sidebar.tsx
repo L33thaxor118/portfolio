a8fd4c1e0dcb511d21d0b6a3201a7652fa0a130b
Azriel Alvarado <aalvarado@fulcrumgt.com>
2021-12-27 02:31:19 -0600

    Add changing profile image

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,13 +2,31 @@
 import { jsx } from '@emotion/react'
 import * as Style from './styles'
 import React, { useState } from 'react'
-import profileImage from '../../assets/alien.jpeg'
+
 import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
+  const [currImage, setCurrImage] = useState("alien.jpeg")
+
+  function meOrBlue(): "me" | "blue" {
+    return (Math.random()*100)>50 ? "me" : "blue"
+  }
   return (
     <div css={Style.sidebar}>
-      <img css={Style.profileImg} src={profileImage}/>
+      <div style={{display: 'flex'}}>
+        {
+          currImage === "alien_blue.png" && <p>That's my dog</p>
+        }
+        <img 
+          css={Style.profileImg} 
+          src={currImage} 
+          onMouseEnter={()=>{setCurrImage(`alien_${meOrBlue()}.png`)}}
+          onMouseLeave={()=>{setCurrImage("alien.jpeg")}}
+          />
+        {
+          currImage === "alien_me.png" && <p>That's me</p>
+        }
+      </div>
       <Link css={Style.listItemText} to="/">whoami</Link>
       <Link css={Style.listItemText} to="/projects">projects</Link>
     </div>
