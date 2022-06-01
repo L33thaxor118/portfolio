7cd6bcb789bf489f35b692b1b2695a132394fc15
Azriel Alvarado <alvarado_118@yahoo.com>
2022-05-31 23:23:37 -0500

    Add contact icons

--- src/components/content/whoami/WhoAmI.tsx
+++ src/components/content/whoami/WhoAmI.tsx
@@ -2,15 +2,28 @@
 import { jsx } from '@emotion/react'
 import React from 'react'
 import * as Style from './styles'
+import { AiOutlineLinkedin } from 'react-icons/ai';
+import { AiOutlineMail } from 'react-icons/ai'
+import { FaGithubSquare } from 'react-icons/fa'
+import { VscGithub } from 'react-icons/vsc'
 
 export default function WhoAmI() {
   return (
     <div css={Style.content}>
-      <h1 css={Style.title}>About Me</h1>
-      <p css={Style.body}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-      <h1 css={Style.title}>Contact</h1>
-      
+      <h1 css={Style.title}>Hi! I'm Azriel</h1>
+      <p css={Style.body}>I'm a Software Developer and I love web + Android. I can code really slow in any language, but I'm pretty quick with Kotlin and Typescript.</p>
+      <h1 css={Style.title2}>Get in touch</h1>
+      <div css={Style.contact}>
+        <a href='https://www.linkedin.com/in/azriel-alvarado-105894182/' target="_blank">
+          <AiOutlineLinkedin css={Style.icon1}/>
+        </a>
+        <a href='https://github.com/L33thaxor118' target="_blank">
+          <VscGithub css={Style.icon2}/>
+        </a>
+        <a href='mailto: alvarado_118@yahoo.com' target="_blank">
+          <AiOutlineMail css={Style.icon1}/>
+        </a>
+      </div>
     </div>
   )
 }
--- src/components/content/whoami/styles.ts
+++ src/components/content/whoami/styles.ts
@@ -7,14 +7,51 @@ export const content = css`
   flex-direction: column;
   align-items: center;
   margin-top: var(--default-padding-top);
+  max-width: var(--text-content-max-width);
 `
-
 export const title = css`
+  font-family: var(--font-family-secondary);
   margin-bottom: 20px;
   color: white;
 `
-
+export const title2 = css`
+  margin-top: 100px;
+  font-family: var(--font-family-secondary);
+  margin-bottom: 20px;
+  color: white;
+`
 export const body = css`
   color: white;
-  font-size: 2vh;
+  font-size: 2.5vh;
+  line-height: var(--text-line-height);
+`
+export const icon1 = css`
+  color: white;
+  width: 90px;
+  height: 90px;
+  opacity: 80%;
+  &:hover {
+    transition: opacity 0.2s;
+    opacity: 100%;
+  }
+  margin-left: 10px;
+  margin-right: 10px;
+  cursor: pointer;
+`
+export const icon2 = css`
+  color: white;
+  width: 80px;
+  height: 80px;
+  opacity: 80%;
+  &:hover {
+    transition: opacity 0.2s;
+    opacity: 100%;
+  }
+  margin-left: 10px;
+  margin-right: 10px;
+  cursor: pointer;
+`
+export const contact = css`
+  display: flex;
+  align-items: center;
 `
