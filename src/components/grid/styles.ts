b20b2e424fc4eedb5c52d87b6e2db3e6b1270f3d
Azriel Alvarado <alvarado_118@yahoo.com>
2022-10-10 20:27:08 -0500

    Increase grid item size

--- src/components/grid/styles.ts
+++ src/components/grid/styles.ts
@@ -14,8 +14,8 @@ export const projectContainer = (src: string) => css`
   flex-grow: 1;
   cursor: pointer;
   color: white;
-  width: 250px;
-  height: 250px;
+  width: 300px;
+  height: 300px;
   display: flex;
   flex-direction: column;
   align-items: center;
